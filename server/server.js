6b47dea0041b31e801e08a6dac03c92d18db745b
Jagannath Hansda <jagannath4698@gmail.com>
2026-06-11 00:12:48 +0530

    faulty backend correction

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'; 
-import cors from 'cors'; //connect backend to any frontend
-import 'dotenv/config'; //we can use environment variable in backend
-import { clerkMiddleware, requireAuth } from '@clerk/express'
+import cors from 'cors'; 
+import 'dotenv/config'; 
+import { clerkMiddleware, requireAuth } from '@clerk/express';
 import aiRouter from './routes/aiRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
@@ -10,21 +10,23 @@ const app = express();
 
 await connectCloudinary();
 
-// app.use(cors({ origin: 'http://localhost:5173' })); //all the request will be passed through this cors
 app.use(cors());
-app.use(express.json()); //all the request will be passed through json
-app.use(clerkMiddleware())
+app.use(express.json()); 
+    app.use(clerkMiddleware());
 
 app.get('/', (req, res) => res.send('Server is live'));
 
-app.use(requireAuth()); // only logedin user can get proceed
+app.use(requireAuth()); 
 
 app.use('/api/ai', aiRouter);
-app.use('/api/user', userRouter)
+app.use('/api/user', userRouter);
 
-const PORT = process.env.PORT || 5000; //if the port is present in environment variable otherwise the port will be 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log('Server is running on port', PORT);
+    });
+}
 
+export default app;
